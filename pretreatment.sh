3df4605a4b40652df614bde3e30761e0a66fa885
ZhichaoDuan <zcduan55@gmail.com>
2019-12-02 17:45:23 +0800

    Create pretreatment.sh

---
+++ pretreatment.sh
@@ -0,0 +1,5 @@
+rm -rf /home/duanzhichao/datasets/GaitDatasetB-silh-my-pretreatment
+python pretreatment.py \
+--worker_num 1 \
+--input_path /home/duanzhichao/datasets/GaitDatasetB-silh-extracted \
+--output_path /home/duanzhichao/datasets/GaitDatasetB-silh-my-pretreatment
