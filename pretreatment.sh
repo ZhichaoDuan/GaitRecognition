d6c95116e82cdc3a3330b6459ea3014b941991b7
ZhichaoDuan <zcduan55@gmail.com>
2019-12-03 12:13:54 +0800

    Update pretreatment.sh

--- pretreatment.sh
+++ pretreatment.sh
@@ -1,5 +1,5 @@
-rm -rf /home/duanzhichao/datasets/GaitDatasetB-silh-my-pretreatment
 python pretreatment.py \
---worker_num 1 \
+--worker_num 8 \
+--use_log \
 --input_path /home/duanzhichao/datasets/GaitDatasetB-silh-extracted \
 --output_path /home/duanzhichao/datasets/GaitDatasetB-silh-my-pretreatment
